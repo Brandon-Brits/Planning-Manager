7d1e8e2fb11ae00023444944420de8fd84546a0f
Brandon Brits <brandon.brits911@gmail.com>
2021-08-16 05:28:13 +0200

    sql GUI

--- SearchResult.java
+++ SearchResult.java
@@ -9,14 +9,14 @@ public class SearchResult implements ActionListener{
 	JFrame frame, frame1;
 	JTextField textbox;
 	JLabel label;
-	Jbutton button;
+	JButton button;
 	JPanel panel;
 	static JTable table;
 	
 	String driverName = "com.mysql.jdbc.Driver";
 	String url = "jdbc:mysql://localhost:3306/record";
 	String userName = "root";
-	String password = "root";
+	String password = "Brit$2212";
 	String[] columnNames = {"Sales Order", "Pump", "Customer", "Date"};
 	
 	public void createUI()
@@ -47,7 +47,7 @@ public class SearchResult implements ActionListener{
 		ShowTableData();
 	}
 	
-	public void showTableData()
+	public void ShowTableData()
 	{
 		
 		frame1 = new JFrame("Sales Order Search Result");
@@ -60,6 +60,59 @@ public class SearchResult implements ActionListener{
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		table.setFillsViewportHeight(true);
 		JScrollPane scroll = new JScrollPane(table);
-		scroll.setHorizontalScrollBarPolicy
+		scroll.setHorizontalScrollBarPolicy(
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+				scroll.setVerticalScrollBarPolicy(
+						JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+				String textvalue = textbox.getText();
+				String so = "";
+				String pump = "";
+				String cus = "";
+				String date = "";
+				
+				try
+				{
+					Class.forName(driverName);
+					Connection con = DriverManager.getConnection(url, userName, password);
+					String sql = "select * from sonum where SalesOrder = "+textvalue;
+					PreparedStatement ps = con.prepareStatement(sql);
+					ResultSet rs = ps.executeQuery();
+					int i = 0;
+					if(rs.next())
+					{
+						so = rs.getString("SalesOrder");
+						pump = rs.getString("Pump");
+						cus = rs.getString("Customer");
+						date = rs.getString("Date");
+						model.addRow(new Object[] {so, pump, cus, date});
+						i++;
+					}
+					if(i<1)
+					{
+						JOptionPane.showMessageDialog(null, "No Record Found", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+					if(i==1)
+					{
+						System.out.println(i + " Record Found");
+					}
+					else
+					{
+						System.out.println(i + " Records Found");
+					}
+				}
+				catch(Exception ex)
+				{
+					JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				frame1.add(scroll);
+				frame1.setVisible(true);
+				frame1.setSize(400, 300);
 	}
+	
+	public static void main(String args[])
+	{
+		SearchResult sr = new SearchResult();
+		sr.createUI();
+	}
+	
 }
